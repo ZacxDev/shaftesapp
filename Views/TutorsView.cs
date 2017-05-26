7dfbd02ce3f82eb295c1b1258a0eed0f217249f7
Alexxmao <alexanderpnmao@gmail.com>
2017-05-25 19:36:13 -0500

    Added tutor view

---
+++ Views/TutorsView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using CoreGraphics;
+using ObjCRuntime;
+
+namespace ShaftesApp.Views
+{
+    static class TutorsView
+    {
+        public TutorsView()
+        {
+        }
+    }
+}
